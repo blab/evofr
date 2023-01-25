34600ea705a482abbe918e100b35911c76b2321d
marlinfiggins <marlinfiggins@gmail.com>
2023-01-25 14:45:15 -0800

    Adding testing data
    Adding hierarchical testing data
    
    Fixing test to use MLR variant counts
    
    Fixing path for testing_data

--- test/data/test_seq_data.py
+++ test/data/test_seq_data.py
@@ -3,20 +3,23 @@ from evofr.data import VariantFrequencies
 from evofr.data.hier_frequencies import HierFrequencies
 
 TEST_SEQ = pd.read_csv(
-    "../rt-from-frequency-dynamics/data/omicron-us-split/omicron-us-split_location-variant-sequence-counts.tsv",
+    "./test/testing_data/mlr-variant-counts.tsv",
     sep="\t",
 )
 
 
 def test_variant_frequencies():
-    filtered_test_seq = TEST_SEQ[TEST_SEQ.location == "Washington"].copy()
-    seq_data = VariantFrequencies(filtered_test_seq)
-    print(seq_data.make_data_dict())
+    filtered_test_seq = TEST_SEQ[TEST_SEQ.location == "City0"].copy()
+    seq_data = VariantFrequencies(filtered_test_seq, pivot="C")
+    data = seq_data.make_data_dict()
+
+    assert data["N"].shape == (70,)
+    assert data["seq_counts"].shape == (70, 3)
 
 
 def test_hier_variant_frequencies():
-    hier_seq_data = HierFrequencies(TEST_SEQ, group="location")
+    hier_seq_data = HierFrequencies(TEST_SEQ, group="location", pivot="C")
     data = hier_seq_data.make_data_dict()
 
-    assert data["seq_counts"].shape == (138, 7, 23)
-    assert data["N"].shape == (138, 23)
+    assert data["seq_counts"].shape == (70, 3, 2)
+    assert data["N"].shape == (70, 2)
