060e2e3847c424d94337772de63acb7df879553d
marlinfiggins <marlinfiggins@gmail.com>
2023-01-25 11:17:27 -0800

    Let user sample from prior

--- evofr/infer/__init__.py
+++ evofr/infer/__init__.py
@@ -4,3 +4,4 @@ from .SVI_handler import SVIHandler
 from .InferMCMC import InferMCMC, InferNUTS
 from .InferSVI import InferSVI, InferMAP, InferFullRank, init_to_MAP
 from .InferBlackJax import InferBlackJax, BlackJaxHandler
+from .SamplePrior import *
