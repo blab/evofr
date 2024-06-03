16465499529db62fd6a3dceb1abbb62596d35eec
marlinfiggins <marlinfiggins@gmail.com>
2024-06-03 12:46:49 -0700

    Updating imports

--- evofr/__init__.py
+++ evofr/__init__.py
@@ -1,17 +1,47 @@
 import importlib.metadata
 
-from .data import (CaseCounts, CaseFrequencyData, DataSpec, HierarchicalCFData,
-                   HierCases, HierFrequencies, VariantFrequencies)
+from .data import (
+    CaseCounts,
+    CaseFrequencyData,
+    DataSpec,
+    HierarchicalCFData,
+    HierCases,
+    HierFrequencies,
+    VariantFrequencies,
+)
 from .data.data_helpers import *
-from .infer import (InferFullRank,  # , BlackJaxHandler; InferBlackJax,
-                    InferMAP, InferMCMC, InferNUTS, InferSVI, MCMCHandler,
-                    SVIHandler, init_to_MAP)
-from .models import (HierMLR, InnovationMLR, InnovationSequenceCounts,
-                     ModelSpec, MultinomialLogisticRegression, PianthamModel)
+from .infer import InferFullRank  # , BlackJaxHandler; InferBlackJax,
+from .infer import (
+    InferMAP,
+    InferMCMC,
+    InferNUTS,
+    InferSVI,
+    MCMCHandler,
+    SVIHandler,
+    init_to_MAP,
+)
+from .models import (
+    HierMLR,
+    InnovationMLR,
+    InnovationSequenceCounts,
+    MLRSpline,
+    ModelSpec,
+    MultinomialLogisticRegression,
+    PianthamModel,
+)
 from .models.renewal_model import *
-from .posterior import (EvofrEncoder, MultiPosterior, PosteriorHandler,
-                        get_freq, get_median, get_quantile, get_quantiles,
-                        get_site_by_variant, get_sites_quantiles_json,
-                        get_sites_variants_json, save_json)
+from .posterior import (
+    EvofrEncoder,
+    MultiPosterior,
+    PosteriorHandler,
+    get_freq,
+    get_median,
+    get_quantile,
+    get_quantiles,
+    get_site_by_variant,
+    get_sites_quantiles_json,
+    get_sites_variants_json,
+    save_json,
+)
 
 __version__ = importlib.metadata.version("evofr")
